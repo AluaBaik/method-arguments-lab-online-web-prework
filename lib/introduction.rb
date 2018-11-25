30ca883efc329f00830d42bc7c9bde9831e95960
baikadamova.alua <baikadamova.alua@gmail.com>
2018-11-25 06:39:53 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction (name)
   "Hi, my name is #{name}."
 end
 
-def introduction_with_language (name,program)
-  "Hi, my name is #{name} and I am learning to program in #{program}."
+def introduction_with_language (name,lang)
+  "Hi, my name is #{name} and I am learning to program in #{lang}."
 end
